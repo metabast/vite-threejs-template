33d97076f9e3bc71fb26075ff6aa21cb6fa2d419
Sébastien <metabast@gmail.com>
2022-10-25 15:57:29 +0200

    clean store

--- src/store/index.js
+++ src/store/index.js
@@ -1,46 +1,10 @@
-import { Color } from "three";
-import { createStore } from "vuex";
+import { Color, Vector3 } from 'three';
+import { createStore } from 'vuex';
 
 const store = createStore({
     state() {
         return {
-            matcapEditor: {
-                sizes: {
-                    view: 200,
-                    exportDefault: 256,
-                    exportRatios: [.5, 1, 2, 4],
-                    exportRatio: 1,
-                },
-                ratio: 256/200,
-                material:{
-                    roughness: .25,
-                    metalness: .5,
-                },
-                ambiant: {
-                    color: new Color(),
-                    intensity: 0, //0.004
-                },
-                create : {
-                    front: true,
-                    lightType: 'Point',
-                    color: 0xFFFFFF,
-                    intensity: 1,
-                    distance:1,
-                    area: {
-                        width: 2,
-                        height: 6,
-                    }
-                },
-                lights : [],
-                isUILightVisible: true,
-            },
-            models: {
-                visible: {
-                    torusKnot: true,
-                    spheres: false
-                },
-                power: 10,
-            },
+            position: new Vector3(0, 0, 0),
         };
     },
     mutations: {
